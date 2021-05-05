ecdb04f0a7af791501c0328f3c67144c2a569ff9
nakagawa.makoto <wwwww.desire@gmail.com>
2021-05-06 00:00:00 +0900

    今日の精進

--- 192/C/main.cpp
+++ 192/C/main.cpp
@@ -1,29 +1,38 @@
+// ゴリ押しで正解
+// digitsを配列に入れてソートしたが、文字列に変換してそれにソート直がけした方が早かったらしい
+// 開設読んでも9下げforと0上げforをあれしてたりしてよくわからない
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
  
 int main() {
-  // 1行に1要素
-  int N;
-  cin >> N;
-  // 1行にスペース区切りで複数
-  int A, B;
-  cin >> A >> B;
-  // N個からなるベクトル
-  vector<int> vec(N);
-  rep(i, N) {
-    cin >> vec.at(i);
+  int N, K;
+  cin >> N >> K;
+  int current = N;
+  for(int i = 0; i < K; i++){
+    int res = 0;
+    vector<int> nums;
+    int c = current;
+    while(c > 0){
+      int dig = c % 10;
+      c = c / 10;
+      nums.push_back(dig);
+    }
+    sort(nums.begin(), nums.end());
+    int g1 = 0;
+    int g2 = 0;
+    for(int j = 0; j < nums.size(); j++){
+      if(j == 0){
+        g2 += nums[nums.size()-1-j];
+        g1 += nums[j];
+        // cout << g1 << "," << g2 << endl;
+      }else {
+        g2 += nums[nums.size()-1-j] * pow(10, j);
+        g1 += nums[j] * pow(10, j);
+        // cout << g1 << "," << g2 << endl;
+      }
+    }
+    current = g1 - g2;
   }
-
-  // 一般的な出力
-  cout << N <<endl;
-  // ベクトルを改行区切りで出力
-  for(auto v: vec) {
-    cout << v << endl;
-  }
-  // ベクトルをスペース区切りで出力
-  rep(i, vec.size()-1) {
-    cout << vec.at(i) << " ";
-  }
-  cout << vec.back() << endl;
+  cout << current << endl;
 }
